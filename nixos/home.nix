f1bea31d3a39f5d50aad534ee174101c8f5c543a
jt1t0r <jt1t0r@proton.me>
2024-11-29 00:19:26 -0500

    mpd conf

--- nixos/home.nix
+++ nixos/home.nix
@@ -50,6 +50,13 @@
     emacs.enable = true;
     newsboat.enable = true;
     home-manager.enable = true;
+    obs-studio = {
+      enable = true;
+      plugins = [
+        pkgs.obs-studio-plugins.wlrobs
+        pkgs.obs-studio-plugins.obs-pipewire-audio-capture
+      ];
+    };
 
     # zsh stuff
     zsh = {
@@ -131,6 +138,9 @@
 
   # iamb source file
   home.file."/.config/iamb/config.toml".source = ./modules/iamb/config.toml;
+
+  # mpd source file
+  home.file."/.config/mpd/mpd.conf".source = ./modules/mpd/mpd.conf;
 
 }
 
